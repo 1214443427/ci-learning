f1e5c7c9e34e9deed809b0789e6cc4804c6d064b
Andrew <a1214443427@gmail.com>
2026-07-10 14:37:50 -0700

    Purposefully breaking the code

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Works on my machine.');
+  res.send('Does not work on my machine.');
 });
 
 module.exports = app;
